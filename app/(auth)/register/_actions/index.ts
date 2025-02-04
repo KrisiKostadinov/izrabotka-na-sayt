bd9a19bdb88cbd2f5ed85d607d1c09aedb71d231
KrisiKostadinov <krisi.199898@gmail.com>
2025-02-04 04:02:14 +0200

    refactor registration logic to remove transaction and streamline user creation process

--- app/(auth)/register/_actions/index.ts
+++ app/(auth)/register/_actions/index.ts
@@ -29,62 +29,58 @@ export const registerUser = async (values: FormSchemaProps) => {
   const token = await generateToken(512);
 
   try {
-    await prisma.$transaction(async (prismaTransaction) => {
-      const createdUser = await prismaTransaction.user.create({
-        data: {
-          email: values.email,
-          password: hashedPassword,
-          role,
-        },
-      });
-
-      await prismaTransaction.token.create({
-        data: {
-          token,
-          userId: createdUser.id,
-          expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
-        },
-      });
-
-      const link = await generateConfirmationLink(createdUser.id, token, "email-confirmation");
-
-      const emailValues = {
-        website_name: process.env.WEBSITE_TITLE || "",
-        website_url: process.env.NEXT_PUBLIC_SITE_URL || "",
-        current_year: new Date().getFullYear().toString() || "",
-        email: values.email || "",
-        link: link,
-        website_email: process.env.NEXT_PUBLIC_SITE_URL || "",
-        support_email: process.env.ADMIN_SUPPORT_EMAIL || "",
-        support_phone: process.env.ADMIN_SUPPORT_PHONE || "",
-      };
-
-      const emailTemplate = await prismaTransaction.emailTemplate.findFirst({
-        where: { key: "email-confirmation" },
-      });
-
-      if (!emailTemplate || emailTemplate.code.length < 10) {
-        throw new Error("Имейл темплейтът не е намерен");
-      }
-
-      const replacedHtml = replaceVariables(emailTemplate.code, emailValues);
-      if (!replacedHtml.success) {
-        throw new Error(replacedHtml.result);
-      }
-
-      const result = await sendEmail({
-        to: values.email,
-        subject: "Потвърждение на имейл",
-        html: replacedHtml.result,
-        allowReply: false,
-      });
-
-      if (result.error) {
-        throw new Error(result.error);
-      }
-
-      return createdUser;
+    const createdUser = await prisma.user.create({
+      data: {
+        email: values.email,
+        password: hashedPassword,
+        role,
+      },
     });
+
+    await prisma.token.create({
+      data: {
+        token,
+        userId: createdUser.id,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    });
+
+    const link = await generateConfirmationLink(createdUser.id, token, "email-confirmation");
+
+    const emailValues = {
+      website_name: process.env.WEBSITE_TITLE || "",
+      website_url: process.env.NEXT_PUBLIC_SITE_URL || "",
+      current_year: new Date().getFullYear().toString() || "",
+      email: values.email || "",
+      link: link,
+      website_email: process.env.NEXT_PUBLIC_SITE_URL || "",
+      support_email: process.env.ADMIN_SUPPORT_EMAIL || "",
+      support_phone: process.env.ADMIN_SUPPORT_PHONE || "",
+    };
+
+    const emailTemplate = await prisma.emailTemplate.findFirst({
+      where: { key: "email-confirmation" },
+    });
+
+    if (!emailTemplate || emailTemplate.code.length < 10) {
+      throw new Error("Имейл темплейтът не е намерен");
+    }
+
+    const replacedHtml = replaceVariables(emailTemplate.code, emailValues);
+    if (!replacedHtml.success) {
+      throw new Error(replacedHtml.result);
+    }
+
+    const result = await sendEmail({
+      to: values.email,
+      subject: "Потвърждение на имейл",
+      html: replacedHtml.result,
+      allowReply: false,
+    });
+
+    if (result.error) {
+      throw new Error(result.error);
+    }
 
     return { message: "Профилът беше създаден" };
   } catch (error) {
